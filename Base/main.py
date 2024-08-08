5fb520c007cb03c0d41a467dd94962d240c38734
Mscode04 <ms.shaheenkp@gmail.com>
2024-08-08 17:19:30 +0530

    completed

---
+++ Base/main.py
@@ -0,0 +1,47 @@
+def print_house():
+    print("    *    ")
+    print("   ***   ")
+    print("  *****  ")
+    print(" ******* ")
+    print("*********")
+
+    print("*       *")
+    print("*       *")
+    print("*       *")
+    print("*       *")
+    print("*********")
+
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print_house()
+
+
+def print_python_logo():
+    logo = [
+        "       ****       ****       ",
+        "     **    **   **    **     ",
+        "   **        ** **        **   ",
+        " **            *            ** ",
+        "**                          **",
+        "**                          **",
+        "**                          **",
+        " **                        ** ",
+        "   **                    **   ",
+        "     **                **     ",
+        "       ****        ****       ",
+        "           **    **           ",
+        "             ****             "
+    ]
+
+    for line in logo:
+        print(line)
+
+print_python_logo()
+
